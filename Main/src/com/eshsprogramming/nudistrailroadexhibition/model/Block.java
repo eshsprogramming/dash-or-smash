cd6ee7581663f7b6971fafda257d5307cd4b8b8c
Zachary Latta <zchlatta@gmail.com>
2012-12-24 01:08:15 -0800

    Added block class.

---
+++ Main/src/com/eshsprogramming/nudistrailroadexhibition/model/Block.java
@@ -0,0 +1,56 @@
+package com.eshsprogramming.nudistrailroadexhibition.model;
+
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+
+/**
+ * @author Zachary Latta
+ */
+public class Block
+{
+	/**
+	 * The block's size
+	 */
+	public static final float SIZE = 1f;
+
+	/**
+	 * The block's position in the world.
+	 */
+	private Vector2 position = new Vector2();
+	/**
+	 * The bounds of the block
+	 */
+	private Rectangle bounds = new Rectangle();
+
+	/**
+	 * Creates a new block.
+	 *
+	 * @param position The block's position in the world.
+	 */
+	public Block(Vector2 position)
+	{
+		this.position = position;
+		this.bounds.width = SIZE;
+		this.bounds.height = SIZE;
+	}
+
+	/**
+	 * Returns the bounds of the block
+	 *
+	 * @return The bounds of the block
+	 */
+	public Rectangle getBounds()
+	{
+		return bounds;
+	}
+
+	/**
+	 * Returns the position of the block
+	 *
+	 * @return The position of the block
+	 */
+	public Vector2 getPosition()
+	{
+		return position;
+	}
+}
