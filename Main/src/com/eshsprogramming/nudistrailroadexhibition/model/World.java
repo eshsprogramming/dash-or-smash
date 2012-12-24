31c2508427aebb59894be07f413a88b8c3ae0a21
Zachary Latta <zchlatta@gmail.com>
2012-12-24 01:43:55 -0800

    Fixed displaying of blocks and nudists.

--- Main/src/com/eshsprogramming/nudistrailroadexhibition/model/World.java
+++ Main/src/com/eshsprogramming/nudistrailroadexhibition/model/World.java
@@ -27,13 +27,13 @@ public class World
 		// Adds nudists to the array of them
 		for(int index = 0; index < 3; index++)
 		{
-			nudists.add(new Nudist(new Vector2(index, 3)));
+			nudists.add(new Nudist(new Vector2(index, 0)));
 		}
 
 		// Adds blocks to the world
-		for(int index1 = 0; index1 < 5; index1++)
+		for(int index1 = 0; index1 < 8; index1++)
 		{
-			for(int index2 = 0; index2 < 8; index2++)
+			for(int index2 = 0; index2 < 5; index2++)
 			{
 				blocks.add(new Block(new Vector2(index1, index2)));
 			}
