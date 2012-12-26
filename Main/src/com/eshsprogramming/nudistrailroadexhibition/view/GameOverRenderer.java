c0279a3dd9cbd4b2163dcf9718030835334f26ed
Benjamin Landers <benjaminrlanders@gmail.com>
2012-12-26 05:18:38 -0800

    made a framework for the gameover
    note: not finished only started

---
+++ Main/src/com/eshsprogramming/nudistrailroadexhibition/view/GameOverRenderer.java
@@ -0,0 +1,110 @@
+package com.eshsprogramming.nudistrailroadexhibition.view;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.eshsprogramming.nudistrailroadexhibition.model.Block;
+import com.eshsprogramming.nudistrailroadexhibition.model.GameOver;
+import com.eshsprogramming.nudistrailroadexhibition.model.MainMenu;
+
+/**
+ * @author Benjamin Landers
+ */
+public class GameOverRenderer
+{
+
+
+	/**
+	 * The width of the screen in relative units.
+	 */
+	public static final float CAMERA_WIDTH = 8f;
+	/**
+	 * The height of the screen in relative units.
+	 */
+	public static final float CAMERA_HEIGHT = 5f;
+
+	/**
+	 * The GameOver instance.
+	 */
+	private GameOver gameOver;
+
+	/**
+	 * The sprite batch. Used for rendering sprites.
+	 */
+	private SpriteBatch spriteBatch;
+
+	/**
+	 * The texture for the blocks.
+	 */
+	private Texture blockTexture;
+
+	/**
+	 * The width of the screen in pixels.
+	 */
+	private int width;
+	/**
+	 * The height of the screen in pixels.
+	 */
+	private int height;
+	/**
+	 * Pixels per unit on the X axis
+	 */
+	private float ppuX;
+	/**
+	 * Pixels per unit on the Y axis
+	 */
+	private float ppuY;
+	/**
+	 * Creates a new GameOverRenderer
+	 */
+	public GameOverRenderer(GameOver gameOver)
+	{
+		this.gameOver = gameOver;
+		this.spriteBatch = new SpriteBatch();
+	}
+	/**
+	 * Renders the contents of main menu renderer.
+	 *
+	 * @param delta The time in milliseconds between frames.
+	 */
+	public void render(float delta)
+	{
+		spriteBatch.begin();
+		drawBlocks();
+		drawText();
+		spriteBatch.end();
+	}
+
+	/**
+	 * Draws blocks on the screen.
+	 */
+	private void drawBlocks()
+	{
+		for(Block block : gameOver.getBlocks())
+		{
+			spriteBatch.draw(blockTexture, block.getPosition().x * ppuX, block.getPosition().y * ppuY,
+					Block.SIZE * ppuX, Block.SIZE * ppuY);
+		}
+	}
+
+	/**
+	 * Draws the text onto the screen.
+	 */
+	private void drawText()
+	{
+		gameOver.getMessageText().render(spriteBatch, ppuX, ppuY);
+		gameOver.getScoreText().render(spriteBatch, ppuX, ppuY);
+	}
+	/**
+	 * Sets the size of the window in pixels.
+	 *
+	 * @param width  The width of the window in pixels.
+	 * @param height The height of the window in pixels.
+	 */
+	public void setSize(int width, int height)
+	{
+		this.width = width;
+		this.height = height;
+		ppuX = (float) width / CAMERA_WIDTH;
+		ppuY = (float) height / CAMERA_HEIGHT;
+	}
+}
