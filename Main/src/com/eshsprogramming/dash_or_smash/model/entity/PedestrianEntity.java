9ff25ebbb52cdf74750577c078f5a4148d431260
Zachary Latta <zchlatta@gmail.com>
2012-12-29 10:59:32 -0800

    Updated Javadoc

--- Main/src/com/eshsprogramming/dash_or_smash/model/entity/PedestrianEntity.java
+++ Main/src/com/eshsprogramming/dash_or_smash/model/entity/PedestrianEntity.java
@@ -3,14 +3,14 @@ package com.eshsprogramming.dash_or_smash.model.entity;
 import com.badlogic.gdx.math.Vector2;
 
 /**
- * A nudist. Pedestrians can either be dancing or dying. Their position should be where the user moves them.
+ * A pedestrian. Pedestrians can either be idle or dying. Their position should be where the user moves them.
  *
  * @author Zachary Latta
  */
 public class PedestrianEntity extends Entity
 {
 	/**
-	 * The different states the nudist can be in.
+	 * The different states the pedestrian can be in.
 	 */
 	public enum State
 	{
@@ -18,23 +18,23 @@ public class PedestrianEntity extends Entity
 	}
 
 	/**
-	 * The nudist's width in units
+	 * The pedestrian's width in units
 	 */
 	public static final float SIZEX = 0.4f;
 	/**
-	 * The nudist's height in units
+	 * The pedestrian's height in units
 	 */
 	public static final float SIZEY = 0.6f;
 
 	/**
-	 * The nudist's current state
+	 * The pedestrian's current state
 	 */
 	private State state = State.IDLE;
 
 	/**
-	 * Creates a new nudist.
+	 * Creates a new pedestrian.
 	 *
-	 * @param position  The nudist's starting position.
+	 * @param position  The pedestrian's starting position.
 	 * @param imageType The type of image to use while rendering
 	 */
 	public PedestrianEntity(Vector2 position, int imageType)
@@ -43,9 +43,9 @@ public class PedestrianEntity extends Entity
 	}
 
 	/**
-	 * Returns the state of the nudist.
+	 * Returns the state of the pedestrian.
 	 *
-	 * @return The current state of the nudist.
+	 * @return The current state of the pedestrian.
 	 */
 	public State getState()
 	{
@@ -53,9 +53,9 @@ public class PedestrianEntity extends Entity
 	}
 
 	/**
-	 * Sets the state of the nudist.
+	 * Sets the state of the pedestrian.
 	 *
-	 * @param state The new state of the nudist.
+	 * @param state The new state of the pedestrian.
 	 */
 	public void setState(State state)
 	{
